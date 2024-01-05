3f79f7a5fd7fa06f6a8ffe74612811eb21aee37e
Gerardo Sanchez Hernandez <yerald231ger@Gerardos-MacBook-Pro.local>
2024-01-04 19:45:44 -0600

    JC TextField, Snackbar and button

--- app/src/main/java/com/example/myjpc/JCTextFieldButtonsSnackBar.kt
+++ app/src/main/java/com/example/myjpc/JCTextFieldButtonsSnackBar.kt
@@ -6,38 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 class JCTextFieldButtonsSnackBar : ComponentActivity() {
 
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -47,8 +37,11 @@ class JCTextFieldButtonsSnackBar : ComponentActivity() {
             Scaffold(
                 snackbarHost = { SnackbarHost(snackBarHostState) }) { innerPadding ->
                 Column(
-                    modifier = Modifier.padding(innerPadding),
-                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                        .padding(all = 5.dp),
+                    verticalArrangement = Arrangement.Center
                 ) {
                     TextField(
                         value = textFieldState,
@@ -61,12 +54,16 @@ class JCTextFieldButtonsSnackBar : ComponentActivity() {
                         singleLine = true,
                         modifier = Modifier.fillMaxWidth()
                     )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(onClick = {
-                        scope.launch {
-                            snackBarHostState.showSnackbar("Hello from $textFieldState")
-                        }
-                    }) {
+                    Spacer(
+                        modifier = Modifier.height(16.dp)
+                    )
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {
+                            scope.launch {
+                                snackBarHostState.showSnackbar("Hello $textFieldState")
+                            }
+                        }) {
                         Text(text = "Please greet me")
                     }
                 }
