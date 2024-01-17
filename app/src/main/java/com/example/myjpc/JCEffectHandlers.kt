93a4cbd5042d70473f68a87a5609c94c2d535a5d
yerald231ger <yerald231ger@gmail.com>
2024-01-17 13:18:46 -0600

    Effect Handlers

--- app/src/main/java/com/example/myjpc/JCEffectHandlers.kt
+++ app/src/main/java/com/example/myjpc/JCEffectHandlers.kt
@@ -2,13 +2,36 @@ package com.example.myjpc
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class JCEffectHandlers : ComponentActivity() {
 
@@ -16,15 +39,58 @@ class JCEffectHandlers : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val text: String by remember {
-                mutableStateOf("")
-            }
-
-            LaunchedEffect(key1 = text) {
-                delay(1000L)
-                println("The text is $text")
+            val snackBarHostState = remember { SnackbarHostState() }
+            val scope = rememberCoroutineScope()
+            Scaffold(snackbarHost = { SnackbarHost(snackBarHostState) }) { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                        .padding(all = 5.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    val counter = produceState(initialValue = 0){
+                        delay(3000L)
+                        value = 4
+                    }
+                    if (counter.value % 5 == 0 && counter.value > 0) {
+                        LaunchedEffect(key1 = snackBarHostState){
+                            snackBarHostState.showSnackbar("Counter is $counter")
+                        }
+                    }
+                    Button(onClick = { }) {
+                        Text(text = "Click me ${counter.value}")
+                    }
+                }
             }
         }
 
+    }
+}
+
+
+var i = 0;
+
+@Composable
+fun MyComposable(backPressedDispatcher: OnBackPressedDispatcher) {
+
+    val callback = remember {
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Handle the back button event
+            }
+        }
+    }
+
+    DisposableEffect(key1 = backPressedDispatcher) {
+        backPressedDispatcher.addCallback(callback)
+        onDispose {
+            callback.remove()
+        }
+    }
+
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Click me $i")
     }
 }
